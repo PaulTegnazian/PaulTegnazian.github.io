f6fe1960c1fd823f5a64158a96c3b09f73d2c04b
Paul Tegnazian <paultegnazian@pauls-mbp.myfiosgateway.com>
2020-08-31 18:03:44 -0400

    cleaning up/orgnaizing the gatsby-config file

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,28 +3,31 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
-module.exports = {
-  siteMetadata: {
+module.exports = 
+{
+  siteMetadata: 
+  {
     title: 'Paul Tegnazian'
   },
-  plugins: [
+  plugins:
+  [
+    'gatsby-transformer-remark',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
     {
-      `gatsby-plugin-react-helmet`,
+      resolve: 'gatsby-plugin-typography',
+      options:
+      {
+        pathToConfigModule: 'src/utils/typography'
+      },
     },
     {
       resolve: `gatsby-source-filesystem`,
-      options: {
+      options: 
+      {
         name: `src`,
         path: `${__dirname}/src`,
       },
     },
-    'gatsby-transformer-remark',
-    'gatsby-plugin-emotion',
-    {
-      resolve: 'gatsby-plugin-typography',
-      options: {
-        pathToConfigModule: 'src/utils/typography'
-      }
-    }
   ]
 }
