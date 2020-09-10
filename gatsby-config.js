d4c66d9407897f4cf5850952ab8d157a3df1b51a
Paul Tegnazian <paultegnazian@pauls-mbp.myfiosgateway.com>
2020-09-10 17:34:38 -0400

    gatsby-tutorial-8

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,11 @@ module.exports =
   siteMetadata: 
   {
     title: 'Paul Tegnazian'
+    description: 'A site dedicated to Paul Tegnazian\'s musings.'
+    author: 'Paul Tegnazian'
   },
   plugins:
   [
-    'gatsby-transformer-remark',
-    'gatsby-plugin-emotion',
-    // 'gatsby-plugin-react-helmet', not found in node_modules
     {
       resolve: 'gatsby-plugin-typography',
       options:
@@ -29,5 +28,24 @@ module.exports =
         path: `${__dirname}/src`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: 
+      {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#5e565e`,
+        theme_color: `#5e565e`,
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: `standalone`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-transformer-remark',
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-offline',
+    'gatsby-plugin-react-helmet',
   ]
 }
