2b7686cad15374d877fc472329c12cd267d936a0
Paul Tegnazian <paultegnazian@pauls-mbp.myfiosgateway.com>
2020-09-10 18:12:30 -0400

    gatsby-tutorial 8, try2

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,9 @@ module.exports =
 {
   siteMetadata: 
   {
-    title: 'Paul Tegnazian'
-    description: 'A site dedicated to Paul Tegnazian\'s musings.'
-    author: 'Paul Tegnazian'
+    title: 'Paul Tegnazian',
+    description: 'A civil engineer blogging about life.',
+    author: 'Paul Tegnazian',
   },
   plugins:
   [
@@ -40,7 +40,7 @@ module.exports =
         // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
         // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
         display: `standalone`,
-        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+        icon: `static/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     'gatsby-transformer-remark',
